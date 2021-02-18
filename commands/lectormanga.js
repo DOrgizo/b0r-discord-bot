595be6ce44c7432f96751dd1c193eb80c8cf68a5
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-02-18 17:56:55 -0400

    cambiado setInterval por modulo set-interval

--- commands/lectormanga.js
+++ commands/lectormanga.js
@@ -1,12 +1,14 @@
+const SetInterval = require('set-interval') 
 const fs = require('fs')
 const json = fs.readFileSync('./gayDump.json')
 const gayDump = JSON.parse(json)
-const TIME = 2.4e+6
+const TIME = 3.6e+6 // 60 minutes
+const gayNum = 5142 // number of gay mangas or just gayDump.length
 
 module.exports = msg => {
 
-	interval = setInterval(() => {
-		let rand = Math.floor(Math.random() * 5180)
+	SetInterval.start(() => {
+		let rand = Math.floor(Math.random() * gayNum)
 		msg.channel.send('```' + gayDump[rand].title + '\n\n' + gayDump[rand].description + '```')
-	}, TIME)
+	}, TIME, msg.channel.id)
 }
