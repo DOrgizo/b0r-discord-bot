e34cfae3b1be1aec0fbdebdc81a3cdb4038269e3
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-02-21 03:20:43 -0400

    change arrow function to normal function

--- commands/lectormanga.js
+++ commands/lectormanga.js
@@ -5,7 +5,7 @@ const gayDump = JSON.parse(json)
 const TIME = 5.4e+6 //3.6e+6 = 60 minutes (1 hour), 7.2e+6 = 120 minutes (2 hours), 5.4e+6 = 90 minutes (1.5 hours)
 const gayNum = 5142 // number of gay mangas or just gayDump.length
 
-module.exports = function(activeServers , msg) => {
+module.exports = function(activeServers , msg) {
 
 	if(activeServers[msg.channel.id]) msg.channel.send('**' + "¿Pendiente de que te coja 2 veces maldito marico?.\nYa esta activa la vaina deja la mariquera." + '**')
 	else {	
@@ -15,7 +15,7 @@ module.exports = function(activeServers , msg) => {
 
 			let rand = Math.floor(Math.random() * gayNum)
 			msg.channel.send('```' + gayDump[rand].title + '\n\n' + gayDump[rand].description + '```')
-			
+
 		}, TIME, msg.channel.id)
 
 		activeServers[msg.channel.id] = true
