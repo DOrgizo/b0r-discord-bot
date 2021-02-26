5551a0e9a142e74bbb92f1855d0ba2bea1ea5e7c
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-02-25 23:13:24 -0400

    Delete commands directory

--- commands/echo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'echo',
-	description: "repite lo que el usuario dice",
-	execute(msg, args) {
-		msg.channel.send(args.join(' '))
-	}
-}
--- commands/version.js
+++
@@ -1,9 +0,0 @@
-const {version} = require('../config.json')
-
-module.exports = {
-	name: 'version',
-	description: "Vesion del bot",
-	execute(msg, args) {
-		msg.channel.send(`\`\`${version}\`\``)
-	}
-}
