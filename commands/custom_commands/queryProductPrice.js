f9455a2ac3644b17d6bbd4d91861ca46347db075
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-03-16 11:06:01 -0400

    add Intl.NumberFormat().format()

--- commands/custom_commands/queryProductPrice.js
+++ commands/custom_commands/queryProductPrice.js
@@ -2,21 +2,20 @@ const sqlite3 = require('sqlite3')
 
 module.exports = function queryProductPrice(msg, db) {
 
-	let productConf = msg.content.slice(18).trim().toUpperCase().split(/ +/)
-	let result = undefined
-	const arg = productConf[productConf.length - 1]
+	let productInfo = msg.content.slice(18).trim().toUpperCase().split(/ +/)
+	const arg = productInfo[productInfo.length - 1]
 	const order = {LOW: 'ASC', HIGH: 'DESC'}
  
 	if(arg === 'HIGH' || arg === 'LOW') {
 		 db.all(`
 			SELECT *
 			FROM Product
-			WHERE ProductName LIKE "${productConf.slice(0, productConf.length - 1).join(' ')}%"
+			WHERE ProductName LIKE "${productInfo.slice(0, productInfo.length - 1).join(' ')}%"
 			ORDER BY ProductPrice ${order[arg]}
 			LIMIT 5
 			`, (err, rows) => {
-				result = rows.map(el => {
-					return `${el.ProductName} Bs.S ${el.ProductPrice} ${el.ProductDolarPrice}$ ${el.MarketName}`
+				const result = rows.map(el => {
+					return `${el.ProductName} Bs ${new Intl.NumberFormat().format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
 				})
 				if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
 				else msg.channel.send(`\`\`${result.join('\n')}\`\``)
@@ -26,11 +25,11 @@ module.exports = function queryProductPrice(msg, db) {
 		 db.all(`
 			SELECT *
 			FROM Product
-			WHERE ProductName LIKE "${productConf.join(' ')}%"
+			WHERE ProductName LIKE "${productInfo.join(' ')}%"
 			LIMIT 5
 			`, (err, rows) => {
-				result = rows.map(el => {
-					return `${el.ProductName} Bs.S ${el.ProductPrice} ${el.ProductDolarPrice}$ ${el.MarketName}`
+				const result = rows.map(el => {
+					return `${el.ProductName} Bs ${new Intl.NumberFormat().format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
 				})
 				 if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
 				 else msg.channel.send(`\`\`${result.join('\n')}\`\``)
