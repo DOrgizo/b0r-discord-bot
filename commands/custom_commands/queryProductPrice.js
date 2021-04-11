cb0bb6ee26ba17c236f8012a214fc4054c519c42
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-04-11 01:58:23 -0400

    fixed problem with accents in sql query

--- commands/custom_commands/queryProductPrice.js
+++ commands/custom_commands/queryProductPrice.js
@@ -11,65 +11,36 @@ module.exports = function queryProductPrice(msg, db) {
 		 db.all(`
 			SELECT *
 			FROM Product
-			WHERE ProductName LIKE (?)
+			WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
+			OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
 			ORDER BY ProductPrice ${order[arg]}
 			LIMIT 5
-			`, `${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
+			`, `${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, `%${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
+
 				if(rows === undefined) return
-				let result = rows.map(el => {
-					return `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
-				})
 
-				if(result.length === 0) {
-					db.all(`
-						SELECT * 
-						FROM Product
-						WHERE ProductName LIKE (?)
-						ORDER BY ProductPrice ${order[arg]}
-						LIMIT 5
-						`, `%${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
+				let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
 
-							result = rows.map(el => {
-								return `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
-							})
-
-							if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``) 
-							else msg.channel.send(`\`\`${result.join('\n')}\`\``)
-
-						})
-				}
+				if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
 				else msg.channel.send(`\`\`${result.join('\n')}\`\``)
 			})
 	} else {
 
 		 db.all(`
 			SELECT *
-			FROM Product
-			WHERE ProductName LIKE (?)
+			FROM Product ProductName
+			WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
+			OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
 			LIMIT 5
-			`, `${productInfo.join(' ')}%`, (err, rows) => {
+			`, `${productInfo.join(' ')}%`, `%${productInfo.join(' ')}%`, (err, rows) => {
+
 				if(rows === undefined) return
-				let result = rows.map(el => {
-					return `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
-				})
-				 if(result.length === 0) {
-				 	db.all(`
-				 		SELECT *
-				 		FROM Product
-				 		WHERE ProductName LIKE (?)
-				 		LIMIT 5
-				 		`, `%${productInfo.join(' ')}%`,(err, rows) => {
-				 			result = rows.map(el => {
-				 				return `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`
-				 			})
 
-				 			if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``) 
-				 			else msg.channel.send(`\`\`${result.join('\n')}\`\``)
-				 				
-				 		})
-				 }
+				let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
 
-				 else msg.channel.send(`\`\`${result.join('\n')}\`\``)
+				if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
+				else msg.channel.send(`\`\`${result.join('\n')}\`\``)
+
 			})
 	}
 
