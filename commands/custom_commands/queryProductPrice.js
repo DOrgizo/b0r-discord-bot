32993d39b69c07adf509d94d1c03a2af473795ed
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-04-16 16:59:40 -0400

    Add files via upload

--- commands/custom_commands/queryProductPrice.js
+++ commands/custom_commands/queryProductPrice.js
@@ -23,33 +23,59 @@ module.exports = function queryProductPrice(msg, db) {
 			SELECT *
 			FROM Product
 			WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
-			OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
 			ORDER BY ProductPrice ${order[arg]}
-			LIMIT 5
-			`, `${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, `%${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
+			LIMIT 6
+			`, `${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
 
 				if(rows === undefined) return
 
 				let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
 
-				if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
+				if(result.length === 0) {
+					db.all(`
+						SELECT * FROM Product
+						WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
+						ORDER BY ProductPrice ${order[arg]}
+						LIMIT 6
+						`, `%${productInfo.slice(0, productInfo.length - 1).join(' ')}%`, (err, rows) => {
+							if(rows === undefined) return
+
+							let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
+
+							if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
+							else msg.channel.send(`\`\`${result.join('\n')}\`\``)
+						})
+				}
+				
 				else msg.channel.send(`\`\`${result.join('\n')}\`\``)
 			})
 	} else {
 
 		 db.all(`
-			SELECT *
-			FROM Product ProductName
+			SELECT * FROM Product
 			WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
-			OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
-			LIMIT 5
-			`, `${productInfo.join(' ')}%`, `%${productInfo.join(' ')}%`, (err, rows) => {
+			LIMIT 6
+			`, `${productInfo.join(' ')}%`, (err, rows) => {
 
 				if(rows === undefined) return
 
 				let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
 
-				if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
+				if(result.length === 0) {
+					db.all(`
+						SELECT * FROM Product
+						WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ProductName, "Á", "A"), "É", "E"), "Í", "I"), "Ó", "O"), "Ú", "U") LIKE (?)
+						LIMIT 6
+						`, `%${productInfo.join(' ')}%`, (err, rows) => {
+
+							if(rows === undefined) return
+
+							let result = rows.map(el => `${el.ProductName} Bs ${new Intl.NumberFormat('es-ES').format(el.ProductPrice)} ${el.ProductDolarPrice}$ ${el.MarketName}`)
+
+							if(result.length === 0) msg.channel.send(`\`\`no se mano\`\``)
+							else msg.channel.send(`\`\`${result.join('\n')}\`\``)
+						})
+				}
 				else msg.channel.send(`\`\`${result.join('\n')}\`\``)
 
 			})
