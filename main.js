447103585b28f8e7650294f1267b3a6f4aea1cf6
David Bisbal <54758421+DOrgizo@users.noreply.github.com>
2021-06-21 23:19:14 -0400

    Add colombianos

--- main.js
+++ main.js
@@ -17,23 +17,27 @@ for(const file of commandFiles) {
 	client.commands.set(command.name, command)
 }
 
+//const keyword = { cuesta: true, cuestan: true, vale: true, sale: true } // Para el comando "b0r cuanto cuesta|vale|sale"
+const keyword = new Set(['cuesta', 'cuestan', 'vale', 'valen', 'sale', 'salen'])
+
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}`) )
 
 client.on('message', message => {
 	
-	if(message.content.startsWith("b0r cuanto")){
-		const keyword = { cuesta: true, vale: true, sale: true }
-		const test = message.content.split(/ +/)[2] // nombre temporal hasta que piense en algo mejor o no haga 1 codigo de mierda como este
+	if(message.content.startsWith("b0r cuanto")) {
+		const msg = message.content.trim().split(/ +/)
 
-		if(!keyword[test]) return
+		const test = msg[2] // nombre temporal hasta que piense en algo mejor o no haga 1 codigo de mierda como este
 
+		if(!keyword.has(test) || msg.length < 4) return
 		// ya no necesito declarar la base de datos aqui pero soy retardado y me da flojera acomomodarlo
 		const db = new sqlite3.Database('./commands/custom_commands/scrapingPrices/db1.db')
-		queryProductPrice(message, db)
-	}
 
-	if (!message.content.startsWith(prefix) || message.author.bot) return
+		queryProductPrice(message, db)
+		
+	}	else if(/colomb|colombia/gi.test(message.content) !== false && !message.author.bot) message.channel.send("malditos **colombianos**")
+		else if (!message.content.startsWith(prefix) || message.author.bot) return
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/)
 	const commandName = args.shift().toLowerCase()
